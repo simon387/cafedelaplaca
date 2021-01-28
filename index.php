a7d1576dc0da0c7880ee8cf9d1780a998e74ab7d
simone celia <simone.celia@thinkopen.it>
2021-01-28 23:38:43 +0100

    guardar

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
 <header id="header" class="fixed-top">
 	<div class="container d-flex align-items-center justify-content-between">
 
-		<h1 class="logo"><a href="index.html">Restaurantly</a></h1>
+		<h1 class="logo"><a href="index.php">Restaurantly</a></h1>
 		<!-- Uncomment below if you prefer to use an image logo -->
-		<!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+		<!-- <a href="index.php" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
 		<nav class="nav-menu d-none d-lg-block">
 			<ul>
-				<li class="active"><a href="index.html">Home</a></li>
+				<li class="active"><a href="index.php">Home</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#menu">Menu</a></li>
 				<li><a href="#specials">Specials</a></li>
