2f1bbc3eafeb420feb2046a460edbec07e0c3f34
simone celia <simone.celia@thinkopen.it>
2021-02-07 13:01:40 +0100

    opt-trans

--- util/translate.php
+++ util/translate.php
@@ -8,51 +8,32 @@ function tr($key)
 	if ($lang == '') {
 		$lang = "en";
 	}
-	//in-memory 2d-matrix with all translations, that's a small site(one page)
-	$index = 0;
-	if ($lang == "es") {
-		$index = 1;
+	if ($key == "en") {
+		return $lang;
 	}
-	if ($lang == "ca") {
+	//in-memory 2d-matrix with all translations, that's a small site(one page)
+	$index = 1;
+	if ($lang == "es") {
 		$index = 2;
 	}
-	$matrix = array(//en, es, ca
-		array("Mon-Sun: 08:00 AM - 24:00 PM", "Lun-Dom: 08:00 AM - 24:00 PM", "Dil-Diu: 08:00 AM - 24:00 PM"),//0, timetable
-		array("Menu", "Menú", "Menú"),//1, menu
-		array("Specials", "Especiales", "Especials"),//2, specials
-		array("Events", "Eventos", "Esdeveniments"),//3, events
-		array("Gallery", "Galería", "Galeria"),//4, gallery
-		array("Staff", "Personal", "Personal"),//5, chefs
-		array("Contact", "Contacto", "Contacte"),//6, contact
-		array("Welcome to ", "Bienvenido a ", "Benvingut a "),//7, welcome_to
-		array("Delivering great food for more than 18 years!", "¡Entregando buena comida por más de 18 años!", "Repartint menjar excel·lent durant més de 18 anys!"),//8, subtitle
-		array("Our Menu", "", ""),//9, our_menu
+	if ($lang == "ca") {
+		$index = 3;
+	}
+	$matrix = array(//key, en, es, ca
+		array("timetable", "Mon-Sun: 08:00 AM - 24:00 PM", "Lun-Dom: 08:00 AM - 24:00 PM", "Dil-Diu: 08:00 AM - 24:00 PM"),
+		array("menu", "Menu", "Menú", "Menú"),
+		array("specials", "Specials", "Especiales", "Especials"),
+		array("events", "Events", "Eventos", "Esdeveniments"),
+		array("gallery", "Gallery", "Galería", "Galeria"),
+		array("chefs", "Staff", "Personal", "Personal"),
+		array("contact", "Contact", "Contacto", "Contacte"),
+		array("welcome_to", "Welcome to ", "Bienvenido a ", "Benvingut a "),
+		array("subtitle", "Delivering great food for more than 18 years!", "¡Entregando buena comida por más de 18 años!", "Repartint menjar excel·lent durant més de 18 anys!"),
+		array("our_menu", "Our Menu", "", ""),
 	);
-	//main switch
-	switch ($key) {
-		case "en":
-			return $lang;
-		case "timetable":
-			return $matrix[0][$index];
-		case "menu":
-			return $matrix[1][$index];
-		case "specials":
-			return $matrix[2][$index];
-		case "events":
-			return $matrix[3][$index];
-		case "gallery":
-			return $matrix[4][$index];
-		case "chefs":
-			return $matrix[5][$index];
-		case "contact":
-			return $matrix[6][$index];
-		case "welcome_to":
-			return $matrix[7][$index];
-		case "subtitle":
-			return $matrix[8][$index];
-		case "our_menu":
-			return $matrix[9][$index];
-		default:
-			return $key;
+	for ($i = 0; $i < count($matrix); $i++) {
+		if ($matrix[$i][0] == $key) {
+			return $matrix[$i][$index];
+		}
 	}
 }
