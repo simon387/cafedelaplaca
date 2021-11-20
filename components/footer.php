f26b6d0f57a7cfd224a1441189a6b3bd8c0d2b10
simone <simon387@hotmail.it>
2021-11-20 22:38:24 +0100

    1.0.3

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,8 @@
+<?php
+$f = fopen("changelog.txt", 'r');
+$version = fgets($f);
+fclose($f);
+?>
 <footer id="footer">
 	<div class="footer-top">
 		<div class="container">
@@ -51,12 +56,8 @@
 		</div>
 	</div>
 	<div class="container">
-		<div class="copyright">&copy; Copyright <strong><span>www.simonecelia.it</span></strong>. All Rights Reserved</div>
+		<div class="copyright">&copy; Copyright <strong><span><a href="www.simonecelia.it">www.simonecelia.it</a></span></strong>. All Rights Reserved. <?php echo $version ?></div>
 		<div class="credits">
-			<!-- All the links in the footer should remain intact. -->
-			<!-- You can delete the links only if you purchased the pro version. -->
-			<!-- Licensing information: https://bootstrapmade.com/license/ -->
-			<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ -->
 			Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
 		</div>
 	</div>
---
+++ deploy_localhost.bat
@@ -0,0 +1,2 @@
+rmdir C:\xampp\htdocs\cafedelaplaca /S /Q
+Xcopy /E /I ..\cafedelaplaca\ C:\xampp\htdocs\cafedelaplaca
